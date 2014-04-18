c1766f9f6c5380bb08fe5076d52cc1090ba405a8
Marty Kane <marty@pixotech.com>
2014-04-18 16:33:58 -0500

    Allow cross-domain requests

--- server.js
+++ server.js
@@ -12,10 +12,21 @@ var serverPort = 3000;
 // Connect to the db and init the collections
 var db = mongojs(mongoPath, ['artists','albums'] );
 
+
+//Cross-Domain Requests
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+
 //Set up Server
 var server = restify.createServer({
     name: 'MyMusic',
 });
+server.use( allowCrossDomain );
 server.use( restify.bodyParser( {mapParams: false }) );
 server.use( restify.queryParser({mapParams: false }) );
 
